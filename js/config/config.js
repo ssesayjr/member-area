082b01b7abcc47821deff372b1e0a50a57c3660e
Tikseo <98226512+Tikseo@users.noreply.github.com>
2022-06-21 01:15:43 +0700

    Create config.js

---
+++ js/config/config.js
@@ -0,0 +1,17 @@
+/**
+ * @file config.js
+ * @author Sanjay Sunil
+ * @license MIT
+ */
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCO5asArLj2Lk0aqakrmSYRZquAfDYH2CE",
+  authDomain: "katahacker.firebaseapp.com",
+  databaseURL: "https://katahacker-default-rtdb.firebaseio.com",
+  projectId: "katahacker",
+  storageBucket: "katahacker.appspot.com",
+  messagingSenderId: "790541883770",
+  appId: "1:790541883770:web:d06dac85d83cba87fddec3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
