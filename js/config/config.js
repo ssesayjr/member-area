97b54da19b47ca93c61caaedc009e06c249b9461
ssesayjr <43327507+ssesayjr@users.noreply.github.com>
2023-01-27 15:25:59 -0500

    idk

--- js/config/config.js
+++ js/config/config.js
@@ -4,14 +4,37 @@
  * @license MIT
  */
 
+
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyCO5asArLj2Lk0aqakrmSYRZquAfDYH2CE",
-  authDomain: "katahacker.firebaseapp.com",
-  databaseURL: "https://katahacker-default-rtdb.firebaseio.com",
-  projectId: "katahacker",
-  storageBucket: "katahacker.appspot.com",
-  messagingSenderId: "790541883770",
-  appId: "1:790541883770:web:d06dac85d83cba87fddec3"
+  apiKey: "AIzaSyDSx2SZM_p34N1wiS7Cf4JlsjMxedFE2No",
+  authDomain: "member-area-t4e.firebaseapp.com",
+  databaseURL: "https://member-area-t4e-default-rtdb.firebaseio.com",
+  projectId: "member-area-t4e",
+  storageBucket: "member-area-t4e.appspot.com",
+  messagingSenderId: "969567033672",
+  appId: "1:969567033672:web:81bf5f228f27f7c69e3b7d",
+  measurementId: "G-FP33DCC1ZL"
 };
+
+
+export function getFirebaseConfig() {
+  if (!firebaseConfig || !firebaseConfig.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.js');
+  } else {
+    return firebaseConfig;
+  }
+}
+
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
